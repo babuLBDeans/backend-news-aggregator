88a00265b556cd24e215715f7bbe34e296931c06
Babu Khan <babukhan@evam.com>
2025-02-09 17:38:56 +0500

    job added to schedule with configurable job time interval

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,11 @@
 <?php
-
+namespace App\Jobs;
+use App\Jobs\NewsApiFetchArticlesJob;
+use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Log;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,4 +18,12 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
-    })->create();
+    })
+    ->withSchedule(function (Schedule $schedule) {
+        $interval = env('NEWS_API_INTERVAL', 10); // Get interval from .env file
+        $schedule->call(function (){
+            $articleService = app(\App\Services\ArticleService::class);
+            dispatch(new NewsApiFetchArticlesJob($articleService));
+        })->everyMinute($interval);
+    })
+    ->create();
