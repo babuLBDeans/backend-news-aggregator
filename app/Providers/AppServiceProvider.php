6d10d56894cd1f1674ebb75c86221dae44bba59e
Babu Khan <babukhan@evam.com>
2025-02-14 12:58:09 +0500

    NY times repo interface and repo binding

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,8 @@ use App\Repositories\GuardianAPIRepostory;
 use App\Repositories\GuardianAPIRepostoryInterface;
 use App\Repositories\NewsAPIRepository as RepositoriesNewsAPIRepository;
 use App\Repositories\NewsAPIRepositoryInterface;
+use App\Repositories\NewyorkTimesAPIRepostory;
+use App\Repositories\NewyorkTimesAPIRepostoryInterface;
 use App\Repositories\ServicesInterface;
 use App\Services\ArticleService;
 use Illuminate\Support\ServiceProvider;
@@ -19,6 +21,7 @@ class AppServiceProvider extends ServiceProvider
     {
         $this->app->bind(NewsAPIRepositoryInterface::class, RepositoriesNewsAPIRepository::class);
         $this->app->bind(GuardianAPIRepostoryInterface::class, GuardianAPIRepostory::class);
+        $this->app->bind(NewyorkTimesAPIRepostoryInterface::class, NewyorkTimesAPIRepostory::class);
 
         $this->app->bind(ServicesInterface::class, ArticleService::class);
     }
