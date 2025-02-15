5f591ad004a73e1914b0f3967178604507a840c5
Babu Khan <babukhan@evam.com>
2025-02-15 12:15:24 +0500

    route for front end api created

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\NewsController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+//front end api's
+Route::middleware('api')->group(function () {
+    Route::get('/api/news', [NewsController::class, 'searchArticles']);
 });
